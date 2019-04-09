f90881c9c7ee3871bcd1d9ebbd7bbd031fcb9858
Dmitry <naumenko.dmitry19@gmail.com>
2019-04-09 15:56:36 +0300

    Starting WebConfiguration

--- src/main/java/com/lemmeknow/config/WebSecurityConfiguration.java
+++ src/main/java/com/lemmeknow/config/WebSecurityConfiguration.java
@@ -40,7 +40,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/user/put", "/user/{id}").permitAll()
+                .antMatchers("/user/put").permitAll()
+                .antMatchers("/user/{id}").authenticated()
                 .anyRequest().authenticated();
     }
 }
