893ac2eab68cad0c61ba8d0110f24c5f65bc24e0
ArtemSglv <stiw00@bk.ru>
2019-05-04 14:52:39 +0300

    Update deploy.sh
    add .jar extension to filename

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 echo 'Copy files...'
 
 scp -i ~/.ssh/id_rsa \
-    target/SecurityService-0.0.1-SNAPSHOT \
+    target/SecurityService-0.0.1-SNAPSHOT.jar \
     $REMOTEUSER@$REMOTEHOST:$REMOTEAPPDIR
 
 echo 'Bye'
