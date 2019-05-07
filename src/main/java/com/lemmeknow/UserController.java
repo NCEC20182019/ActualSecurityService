8e9639103094adaf32e602d214a96b75de9c4184
Dmitry <naumenko.dmitry19@gmail.com>
2019-05-07 15:42:25 +0300

    Minor changes on redundant code

--- src/main/java/com/lemmeknow/UserController.java
+++ src/main/java/com/lemmeknow/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import java.security.Principal;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
