7188611391f12895f330320178f2ba777dd8e1cf
Dmitry <naumenko.dmitry19@gmail.com>
2019-05-26 17:05:39 +0300

    DB redirect front uri changed

--- src/main/resources/db/migration/V2__Initial_data.sql
+++ src/main/resources/db/migration/V2__Initial_data.sql
@@ -4,7 +4,7 @@ INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_
     ('parser_service', '{bcrypt}$2a$12$HU2SKOk7O9ubgAv9HHUDt.uTT2xL6svF2DPF1szFpDFeOpkAh6Gwa', 'http://localhost:8094', 'profile', 3600, 10000, 'events', 'client_credentials'),
     ('scheduler_service', '{bcrypt}$2a$12$.8M7UW7ruyS8QQ/dNu8lJ.6REyLBToSSyZr/I70ysN.miaLNKSssm', 'http://localhost:8095', 'profile', 3600, 10000, 'events', 'client_credentials'),
     ('notification_service', '{bcrypt}$2a$10$gPhlXZfms0EpNHX0.HHptOhoFD1AoxSr/yUIdTqA8vtjeP4zi0DDu', 'http://localhost:8093', 'profile', 3600, 10000, 'users', 'authorization_code,password,refresh_token,implicit,client_credentials'),
-    ('client_service', '{bcrypt}$2a$12$fvO3jGsXa4yNoAq1YbuTNO6YfJ/qNdS9zug9aZ8V0b6Ry.7ACwCa6', 'http://localhost:80', 'profile, user', 3600, 10000, 'users', 'authorization_code,password,refresh_token,implicit,client_credentials')
+    ('client_service', '{bcrypt}$2a$12$fvO3jGsXa4yNoAq1YbuTNO6YfJ/qNdS9zug9aZ8V0b6Ry.7ACwCa6', 'http://localhost:8090', 'profile, user', 3600, 10000, 'users', 'authorization_code,password,refresh_token,implicit,client_credentials')
 ;
 
  INSERT INTO PERMISSION (NAME)
